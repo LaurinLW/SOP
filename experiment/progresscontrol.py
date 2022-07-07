81b6fcff1a5bfc4e026a92dd4682c2c42fbfb4ef
Niklas Maier <ufxvc@student.kit.edu>
2022-07-07 21:26:57 +0200

    add some class definitions

---
+++ experiment/progresscontrol.py
@@ -0,0 +1,49 @@
+import experiment
+import export
+import connection.serverconnection as sc
+
+
+class ProgressControl:
+    """This class keeps track of the experiments progress.
+    An Export instance should update an instance of this class after each export.
+    This class is also responsible for sending the progress to the server.
+    After the last export the ProgressControl notifies the Experiment that it should stop via the stop()-method.
+    """
+
+    def __init__(self, experiment: experiment.Experiment, num_model_runs: dict[str, int], server: sc.ServerConnection, exporter: export.Exporter) -> None:
+        """contructor
+
+        Args:
+            experiment (experiment.Experiment): running experiment
+            num_model_runs (dict[str, int]): dictionary with model names as key and number of runs as value
+            server (ServerConnection): ServerConnection object
+            exporter (Exporter): exporter that notifies
+        """
+        pass
+
+    def update(self, model: str, subspace_dim: list[str]) -> None:
+        """Exporters should call this method to update the progress of the Experiment.
+
+        Args:
+            model (str): model which results were exported
+            subspace_dim (list[str]): dimension titles of the subspace
+        """
+        pass
+
+    def update_error(self, model: str, subspace_dim: list[str], error: Exception) -> None:
+        """Exporters should call this method to notify Progresscontrol about errors
+
+        Args:
+            model (str): model which results would have been exported
+            subspace_dim (list[str]): dimension titles of the subspace
+            error (Exception): error that occured
+        """
+        pass
+
+    def get_progress() -> int:
+        """returns progress
+
+        Returns:
+            int: progress in percent as integer
+        """
+        pass
