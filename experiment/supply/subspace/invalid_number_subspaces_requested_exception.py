04f35c329d211812edf91ea2b3e9d411527f23c9
ufxvc <ufxvc@student.kit.edu>
2022-09-12 22:37:07 +0200

    Endless loop

---
+++ experiment/supply/subspace/invalid_number_subspaces_requested_exception.py
@@ -0,0 +1,8 @@
+
+"""
+Exception that is raised if more subspaces are requested than the amount that is able to be created
+"""
+
+
+class InvalidNumberSubspacesRequestedException(Exception):
+    pass
--- experiment/supply/subspace/subspace_generator.py
+++ experiment/supply/subspace/subspace_generator.py
@@ -1,10 +1,14 @@
 import numpy as np
+import math
 
 from pandas import DataFrame
 from random import Random
 
 from experiment.supply.preprocessing.cleaner import Cleaner
 from experiment.supply.preprocessing.encoder import Encoder
+from experiment.supply.subspace.invalid_number_subspaces_requested_exception import (
+    InvalidNumberSubspacesRequestedException,
+)
 from experiment.supply.subspace.subspace import Subspace
 
 
@@ -34,14 +38,26 @@ class SubspaceGenerator:
         """
 
         self.__number_subspaces: int = number_subspaces
-        self.__min_dimension: int = min_dimension
-        self.__max_dimension: int = max_dimension + 1
         self.__random_instance: Random = Random()
         self.__random_instance.seed(seed)
-        self.__cleanedData: DataFrame = cleaner.clean(data)
+        self.__cleaned_data: DataFrame = cleaner.clean(data)
+
         self.__encoder: Encoder = encoder
         self.__quantity_created_subspaces: int = 0
-        self.created_subspaces_dimensions: list[list[int]] = list()
+        self.__amount_cleaned_data_columns: int = len(self.__cleaned_data.columns)
+
+        self.__min_dimension = min(min_dimension, self.__amount_cleaned_data_columns)
+        self.__max_dimension = min(max_dimension + 1, self.__amount_cleaned_data_columns + 1)
+
+        self.__created_subspaces_dimensions: dict[int, list[list[int]]] = dict()  # {(i, list()) for i in range(self.__min_dimension, self.__max_dimension)}
+        for i in range(self.__min_dimension, self.__max_dimension):
+            self.__created_subspaces_dimensions[i] = list()
+        self.__possibilites_for_dimension: list[
+            list[int]
+        ] = self.__calculate_possibilites_for_dimension_entry()
+        self.__max_number_subspaces = 0
+        for p in self.__possibilites_for_dimension:
+            self.__max_number_subspaces += p[1]
 
     def __next__(self) -> Subspace:
         """creates a new Subspace and returns it
@@ -53,11 +69,13 @@ class SubspaceGenerator:
             Subspace: returns the newly created Subspace
         """
         if self.__quantity_created_subspaces < self.__number_subspaces:
-            subspace_index: np.ndarray = self.__cleanedData.index.to_numpy()
-            subspace_dimensions = self.__create_unique_subspace_dimensions()
-            self.created_subspaces_dimensions.append(subspace_dimensions)
+            subspace_index: np.ndarray = self.__cleaned_data.index.to_numpy()
+            try:
+                subspace_dimensions = self.__create_unique_subspace_dimensions()
+            except InvalidNumberSubspacesRequestedException as e:
+                raise e
             subspace_encoded: DataFrame = self.__encoder.encode(
-                self.__cleanedData.iloc[:, subspace_dimensions]
+                self.__cleaned_data.iloc[:, subspace_dimensions]
             )
             subspace_dimensions_names: str = list(subspace_encoded.columns)
             subspace_array: np.ndarray = subspace_encoded.to_numpy()
@@ -66,16 +84,43 @@ class SubspaceGenerator:
         raise StopIteration
 
     def __create_unique_subspace_dimensions(self) -> list[int]:
+        if not self.__possibilites_for_dimension:
+            raise InvalidNumberSubspacesRequestedException
+
+        chosen_dimension_indice: int = self.__random_instance.randint(
+            0, len(self.__possibilites_for_dimension) - 1
+        )
+
+        chosen_dimension: list[int] = self.__possibilites_for_dimension[
+            chosen_dimension_indice
+        ]
+
+        chosen_dimension[1] -= 1
+
+        if chosen_dimension[1] == 0:
+            self.__possibilites_for_dimension.remove(chosen_dimension)
+
+        amount_dimensions: int = chosen_dimension[0]
+
         while True:
-            amount_dimensions: int = self.__random_instance.randrange(
-                self.__min_dimension, self.__max_dimension
-            )
             subspace_dimensions: list[int] = self.__random_instance.sample(
-                range(0, len(self.__cleanedData.columns)), amount_dimensions
+                range(0, len(self.__cleaned_data.columns)), amount_dimensions
             )
             subspace_dimensions.sort()
-            if subspace_dimensions not in self.created_subspaces_dimensions:
+            if subspace_dimensions not in self.__created_subspaces_dimensions[amount_dimensions]:
+                self.__created_subspaces_dimensions[amount_dimensions].append(subspace_dimensions)
                 return subspace_dimensions
+
+    def __calculate_possibilites_for_dimension_entry(self) -> list[list[int]]:
+        output: list[list[int]] = list()
+        for i in range(self.__min_dimension, self.__max_dimension):
+            possibilites_for_dimension_entry: list[int] = list()
+            possibilites_for_dimension_entry.append(i)
+            possibilites_for_dimension_entry.append(
+                math.comb(self.__amount_cleaned_data_columns, i)
+            )
+            output.append(possibilites_for_dimension_entry)
+        return output
 
     def __iter__(self) -> object:
         """returns itself to be iterable
@@ -84,3 +129,6 @@ class SubspaceGenerator:
             object: returns itself to be iterable
         """
         return self
+
+    def get_max_number_subspaces(self) -> int:
+        return self.__max_number_subspaces
